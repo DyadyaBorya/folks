16bc9bdd220643f94806a83a41e0b12d6f023e94
Gielo <hgielo@gmail.com>
2013-07-27 23:37:49 +0200

    Delete CommonProxy.java

--- kGielo/folks/CommonProxy.java
+++
@@ -1,8 +0,0 @@
-package kGielo.folks;
-
-public class CommonProxy {
-	// Client stuff
-    public void registerRenderers() {
-            // Nothing here as the server doesn't render graphics or entities!
-    }
-}
